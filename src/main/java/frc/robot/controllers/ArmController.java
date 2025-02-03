4706e7a4fbbd19ef2a93fd5b3600e39136377e01
Erik Currin <ehc12345678@gmail.com>
2025-02-02 21:28:41 -0500

    Fix triggers on arm

--- src/main/java/frc/robot/controllers/ArmController.java
+++ src/main/java/frc/robot/controllers/ArmController.java
@@ -1,14 +1,15 @@
 package frc.robot.controllers;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Robot;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.muncher.YeetCommand;
-import frc.robot.Robot;
 
 /**
  * This will control both the arm AND the muncher/outtake.
+ * TODO: consider using CommandXBoxController for built in eventHandler conveniences, e.g
+ * rightBumper().onTrue(YeetCommand.create())
  */
 public class ArmController extends XboxController {
     public Boolean armManualControl = false;
@@ -16,13 +17,13 @@ public class ArmController extends XboxController {
     public Boolean rotateArmMotorEnabled = true;
     public ArmController(int port) {
         super(port);
-        leftTrigger(0.25, bind(this::onLeftTrigger));
-        rightTrigger(0.25, bind(this::onRightTrigger));
+        leftTrigger(.25, CommandScheduler.getInstance().getDefaultButtonLoop()).ifHigh(this::onLeftTrigger);
+        rightTrigger(.25, CommandScheduler.getInstance().getDefaultButtonLoop()).ifHigh(this::onRightTrigger);
     }
 
     public void teleopPeriodic() {
         if (Robot.map.muncher != null) {
-            if (getRightBumperButtonPressed() == true) {
+            if (getRightBumperButtonPressed()) {
                 CommandScheduler.getInstance().schedule(YeetCommand.create());
             }
         }
@@ -39,12 +40,6 @@ public class ArmController extends XboxController {
 
     public void onRightTrigger() {
         Robot.map.muncher.intake(-getRightTriggerAxis());
-    }
-
-    private EventLoop bind(Runnable runnable) {
-        EventLoop trigger = new EventLoop();
-        trigger.bind(runnable);
-        return trigger;
     }
 
     public void armManualControl(boolean armManualControl)
