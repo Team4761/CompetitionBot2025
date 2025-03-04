0bcff5812f7c7be1439ce9413af12a3bfb776c0d
TheManBehindTheMemes <alex@merc-law.com>
2025-03-04 18:24:06 -0500

    Update LEDSubsystem.java

--- src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
@@ -34,7 +34,6 @@ public class LEDSubsystem extends SubsystemBase {
     public LEDSubsystem() {
         // Comment out patterns that aren't being used
 
-        
 
         
         leds = new AddressableLED(Constants.LEDS_PORT);
@@ -80,7 +79,7 @@ public class LEDSubsystem extends SubsystemBase {
         // So to prevent you from feeling the agony of choice, I have decided that the left side will have more red.
         /**
         public void periodic() {
-            // this is the pattern, but it is offset to wrap the red around to the back, because BScode screams at me for repeating red. And ONLY red.
+            // this is the pattern, but it is offset to wrap the red around to the back, because BScode doesn't like having more than 10 keys and values.
             LEDPattern base = LEDPattern.steps(Map.of(0, Color.kRed, 0.59375, Color.kWhite, 0.625, Color.kBlack, 0.6875, Color.kWhite, 0.71875, Color.kBlack, 0.75, Color.kWhite, 0.78125, Color.kBlack, 0.84375, Color.kWhite, 0.90625, Color.kBlack, 0.96875, Color.kWhite));
             LEDPattern pattern = base.offsetBy(-9);
             pattern.applyTo(buffer);
@@ -114,33 +113,64 @@ public class LEDSubsystem extends SubsystemBase {
         */
 
 
-        // 2 solid colors that move towards the side opposite from where they started. every time the colors bounce off a wall, the colors change to a new random one (super unfinished)
-        /**
+        // 2 solid colors that move towards the side opposite from where they started. every time the colors bounce off a wall, the colors change to a new random one. (super unfinished)
+        // WARNING: I DO NOT KNOW HOW TO CODE WELL, AND THIS MAY BE COMPLETELY WRONG IN MULTIPLE WAYS. If this is wrong in a massive way, please yell at me for it next time you see me.
+        // Filler
+        // Currently it should create 2 groups of differnt colors, and have them move accross the strip and bounce off walls.
+        // I hope you aren't afraid of unnecessary comments
+        // Oh also I don't know how to blend the patterns, because the WPILib document contains jack-shit about it. So it doesnt actually make a pattern...
+        // Here's another line just to make sure
+        public int LEDOffset = 0;
         public void periodic() {
             // colors always start as Red and White
-            LEDPattern left = LEDPattern.steps(Map.of(0, Color.kRed, 0.125, Color.kBlack));
-            LEDPattern rightBase = LEDPattern.steps(Map.of(0, Color.kWhite, 0.125, Color.kBlack));
-            LEDPattern right = rightBase.reversed();
+            LEDPattern leftBase = LEDPattern.steps(Map.of(0, Color.kRed, 0.125, Color.kBlack));
+            LEDPattern rightBaseBase = LEDPattern.steps(Map.of(0, Color.kWhite, 0.125, Color.kBlack));
+            LEDPattern rightBase = rightBaseBase.reversed();
+            LEDPattern left = leftBase.offsetBy(LEDOffset);
+            LEDPattern right = rightBase.offsetBy(LEDOffset);
             LEDPattern pattern = null; //placeholder
             pattern.applyTo(buffer);
             leds.setData(buffer);
         }
-        */
+
+        // this should constantly move the offset. I can't test this at the moment, so it will probably be wrong.
+        public int LEDOffset1() {
+            int direction = 0;
+            if (LEDOffset < 28) {
+                if(direction == 0) {
+                    LEDOffset = LEDOffset + 1;
+                }
+            }
+            else {
+                direction = 1;
+            }
+            if (LEDOffset > 0) {
+                if (direction == 1) {
+                    LEDOffset = LEDOffset - 1;
+                }
+            }
+            else {
+                direction = 0;
+            }
+            return LEDOffset;
+        }
 
 
         // blinks Blanched Almond when the robot has successfully aligned with an april tag during teleop (unfinished)
-        
+        /**
         public void periodic() {
             LEDPattern base = LEDPattern.solid(Color.kBlanchedAlmond);
             LEDPattern pattern = base.blink(Seconds.of(0.5));
             pattern.applyTo(buffer);
             leds.setData(buffer);
         }
-        
+        */
+
 
 
         // the command that aplies the pattern to the LEDs
         public Command runPattern(LEDPattern pattern) {
             return run(() -> pattern.applyTo(buffer));
         }
+
 }
