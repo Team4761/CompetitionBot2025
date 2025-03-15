cbbac8f7d67a561d41a049356a37ea9274187a2c
TheManBehindTheMemes <alex@merc-law.com>
2025-03-15 16:36:26 -0400

    LEDS!!!!!!!!!!!!!!!!!!!
    IT FINNALLY WORKSD!!!!!11!!1!!!!!1

--- src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
@@ -148,24 +148,21 @@ public class LEDSubsystem extends SubsystemBase {
             new StupidColor(Color.kYellow),
             new StupidColor(Color.kYellowGreen)
             };
+            
+            int LEDColorIndex1 = (int)(Math.random() * (117 - 0 + 1) + 0);
+            int LEDColorIndex2 = (int)(Math.random() * (117 - 0 + 1) + 0);
+            StupidColor LEDColor1 = LEDColorList[LEDColorIndex1];
+            StupidColor LEDColor2 = LEDColorList[LEDColorIndex2];
+
     // Also, per LED strip, there are 150 LEDs.
     // Supposedly the LEDs function in GRB not RGB... We'll need to test this though.
-    /** what pattern should I use for LEDs?
-     * <p> nominate your LED pattern below (keep in mind, this is a one-dimesional strip) Current nomintaions are:
+    /** Available LED patterns
      * <p> gradient
-     * <p> 4761 in binary
-     * <p> "Win" in morse code
-     * <p> lights that cross over eachother in the middle, then the colors xor
+     * <p> lights that move across the strip, and change to a random color when bounce of the edge
      * <p> lights that blink blanched almond when the robot is perfectly aligned in teleop (the only one with a debugging function)
      */
     public LEDSubsystem() {
         // Comment out patterns that aren't being used
-
-
-        int LEDColorIndex1 = (int)Math.random() * LEDColorList.length;
-        int LEDColorIndex2 = (int)Math.random() * LEDColorList.length;
-        StupidColor LEDColor1 = LEDColorList[LEDColorIndex1];
-        StupidColor LEDColor2 = LEDColorList[LEDColorIndex2];
         
 
         leds = new AddressableLED(Constants.LEDS_PORT);
@@ -175,9 +172,6 @@ public class LEDSubsystem extends SubsystemBase {
         
         currentPattern = RobocketsLEDPatterns.OFF;
         previousPattern = RobocketsLEDPatterns.OFF;
-    }
-    public static void main(String[] args) {
-        System.out.println(LEDColorList[0]);
     }
 
 
@@ -198,14 +192,13 @@ public class LEDSubsystem extends SubsystemBase {
             leds.setData(buffer); 
         }
 
-    /**
-    // progress bar fills proportional to how much the move joystick is pushed (kinda)
+    
 
-    public double getProgress() {
-        progressOfLEDs = Math.abs(Robot.driveController.getLeftX() + -Robot.driveController.getLeftY());
-        return progressOfLEDs;
-    }
-    */
+        public double getProgress() {
+            progressOfLEDs = Math.abs(Robot.driveController.getLeftX() + -Robot.driveController.getLeftY());
+            return progressOfLEDs;
+        }
+        */
 
 
 
@@ -220,108 +213,44 @@ public class LEDSubsystem extends SubsystemBase {
     */
 
 
-    // 4761 in binary
-    // for a 13 bit integer, 4761 would be 1001010011001, and unused LEDs are red.
-    // However, this is an odd number of digits, meaning that the amount of red on each side will be uneven.
-    // So to prevent you from feeling the agony of choice, I have decided that the left side will have more red.
-    /**
-    public void periodic() {
-        // this is the pattern, but it is offset to wrap the red around to the back, because BScode screams at me for repeating red. And ONLY red.
-        LEDPattern base = LEDPattern.steps(Map.of(0, Color.kRed, 0.59375, Color.kWhite, 0.625, Color.kBlack, 0.6875, Color.kWhite, 0.71875, Color.kBlack, 0.75, Color.kWhite, 0.78125, Color.kBlack, 0.84375, Color.kWhite, 0.90625, Color.kBlack, 0.96875, Color.kWhite));
-        LEDPattern pattern = base.offsetBy(-9);
-        pattern.applyTo(buffer);
-        leds.setData(buffer);
-    }
-    */
-
-
-    // Win in morse code
-    /**
-    public void periodic() {
-        // red is unused LEDs
-        // 101101100101001101, 18 digits
-        LEDPattern base = LEDPattern.steps(Map.ofEntries(
-            Map.entry(1, Color.kBlanchedAlmond); OMG IT'S BLANCHED ALMOND, THE BEST COLOR
-            Map.entry(0, Color.kRed),
-            Map.entry(0.4375, Color.kWhite),
-            Map.entry(0.46875, Color.kBlack),
-            Map.entry(0.5, Color.kWhite),
-            Map.entry(0.5625, Color.kBlack),
-            Map.entry(0.59375, Color.kWhite),
-            Map.entry(0.65625, Color.kBlack),
-            Map.entry(0.71875, Color.kWhite),
-            Map.entry(0.75, Color.kBlack),
-            Map.entry(0.78125, Color.kWhite),
-            Map.entry(0.8125, Color.kBlack)));
-        LEDPattern pattern = base.offsetBy(-7);
-        pattern.applyTo(buffer);
-        leds.setData(buffer);
-    }
-    */
-
         
-        // 2 solid colors that move towards the side opposite from where they started. every time the colors bounce off a wall, the colors change to a new random one. (super unfinished)
+        // 2 solid colors that move towards the side opposite from where they started. every time the colors bounce off a wall, the colors change to a new random one. (unfinished)
         // WARNING: I DO NOT KNOW HOW TO CODE WELL, AND THIS MAY BE COMPLETELY WRONG IN MULTIPLE WAYS. If this is wrong in a massive way, please yell at me for it next time you see me.
         // Filler
         // Currently it should create 2 groups of differnt colors, and have them move accross the strip and bounce off walls.
         // I hope you aren't afraid of unnecessary comments
         // Oh also I don't know how to blend the patterns, because the WPILib document contains jack-shit about it. So it doesnt actually make a pattern...
+        // Update: I figured out how to blend them, but we actually need to overlay them instead (which I also figured out)
         // Here's another line just to make sure
+        // ps: I will never delete any of these comments
         
-        public int LEDOffset = 0;
+        public static int LEDOffset = 0;
         public void periodic() {
             // colors always start as random
-            LEDPattern rightBaseBaseBase = LEDPattern.steps(Map.of(0, Color.kWhite, 0.125, Color.kBlack));
-            LEDPattern leftBaseBase = LEDPattern.steps(Map.of(0, Color.kRed, 0.125, Color.kBlack));
+            LEDPattern rightBaseBaseBase = LEDPattern.steps(Map.of(0, LEDColor1, 0.125, Color.kBlack));
+            LEDPattern leftBaseBase = LEDPattern.steps(Map.of( 0, LEDColor2, 0.125, Color.kBlack));
             LEDPattern rightBaseBase = rightBaseBaseBase.reversed();
             LEDPattern leftBase = leftBaseBase.offsetBy(LEDOffset);
-            LEDPattern rightBase = rightBaseBase.offsetBy(LEDOffset);
+            LEDPattern rightBase = rightBaseBase.offsetBy(-LEDOffset);
             LEDPattern left = leftBase;
-            LEDPattern Right = rightBase;
-            LEDPattern pattern = null; //placeholder
+            LEDPattern right = rightBase;
+            LEDPattern pattern = left.overlayOn(right);
             pattern.applyTo(buffer);
             leds.setData(buffer);
-        }
-            
-         
 
-        // this should constantly move the offset. I can't test this at the moment, so it will probably be wrong.
-        /**
-        public int LEDOffset1() {
-            int direction = 0;
-            if (LEDOffset < 28) {
-                if(direction == 0) {
-                    LEDOffset = LEDOffset++;
-                }
+            // should constantly update the offset of the LEDs
+            if (LEDOffset < 132) {
+                LEDOffset++;                
             }
             else {
-                direction = 1;
+                LEDOffset = 0;
+                LEDColorIndex1 = (int)(Math.random() * (117 - 0 + 1) + 0);
+                LEDColorIndex2 = (int)(Math.random() * (117 - 0 + 1) + 0);
+                LEDColor1 = LEDColorList[LEDColorIndex1];
+                LEDColor2 = LEDColorList[LEDColorIndex2];
             }
-            if (LEDOffset > 0) {
-                if (direction == 1) {
-                    LEDOffset = LEDOffset--;
-                }
-            }
-            else {
-                direction = 0;
-            }
-            return LEDOffset;
         }
-        */
 
-
-    // 2 solid colors that move towards the side opposite from where they started. every time the colors bounce off a wall, the colors change to a new random one (super unfinished)
-    /**
-    public void periodic() {
-        // colors always start as Red and White
-        LEDPattern left = LEDPattern.steps(Map.of(0, Color.kRed, 0.125, Color.kBlack));
-        LEDPattern rightBase = LEDPattern.steps(Map.of(0, Color.kWhite, 0.125, Color.kBlack));
-        LEDPattern right = rightBase.reversed();
-        LEDPattern pattern = null; //placeholder
-        pattern.applyTo(buffer);
-        leds.setData(buffer);
-    }
-    */
 
     public void setPattern(LEDPattern pattern) {
         previousPattern = currentPattern;
@@ -355,46 +284,4 @@ public class LEDSubsystem extends SubsystemBase {
     public Command runPattern(LEDPattern pattern) {
         return run(() -> setPattern(pattern));
     }
-
-    // // 2 solid colors that move towards the side opposite from where they started. every time the colors bounce off a wall, the colors change to a new random one. (super unfinished)
-    // // WARNING: I DO NOT KNOW HOW TO CODE WELL, AND THIS MAY BE COMPLETELY WRONG IN MULTIPLE WAYS. If this is wrong in a massive way, please yell at me for it next time you see me.
-    // // Filler
-    // // Currently it should create 2 groups of differnt colors, and have them move accross the strip and bounce off walls.
-    // // I hope you aren't afraid of unnecessary comments
-    // // Oh also I don't know how to blend the patterns, because the WPILib document contains jack-shit about it. So it doesnt actually make a pattern...
-    // // Here's another line just to make sure
-    // public int LEDOffset = 0;
-    // public void periodic() {
-    //     // colors always start as Red and White
-    //     LEDPattern leftBase = LEDPattern.steps(Map.of(0, Color.kRed, 0.125, Color.kBlack));
-    //     LEDPattern rightBaseBase = LEDPattern.steps(Map.of(0, Color.kWhite, 0.125, Color.kBlack));
-    //     LEDPattern rightBase = rightBaseBase.reversed();
-    //     LEDPattern left = leftBase.offsetBy(LEDOffset);
-    //     LEDPattern right = rightBase.offsetBy(LEDOffset);
-    //     LEDPattern pattern = null; //placeholder
-    //     pattern.applyTo(buffer);
-    //     leds.setData(buffer);
-    // }
-
-    // // this should constantly move the offset. I can't test this at the moment, so it will probably be wrong.
-    // public int LEDOffset1() {
-    //     int direction = 0;
-    //     if (LEDOffset < 28) {
-    //         if(direction == 0) {
-    //             LEDOffset = LEDOffset + 1;
-    //         }
-    //     }
-    //     else {
-    //         direction = 1;
-    //     }
-    //     if (LEDOffset > 0) {
-    //         if (direction == 1) {
-    //             LEDOffset = LEDOffset - 1;
-    //         }
-    //     }
-    //     else {
-    //         direction = 0;
-    //     }
-    //     return LEDOffset;
-    // }
 }
